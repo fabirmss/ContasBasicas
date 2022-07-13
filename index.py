926f36f1e3e100b0ddf0ff0a37d04b1a92515182
Fabi <108952843+fabirmss@users.noreply.github.com>
2022-07-13 11:07:17 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+
+a = int(input('Entre com o primeiro valor:'))
+b = int(input('Entre com o segundo valor:'))
+
+soma = a + b
+subtracao = a - b
+divisao = a / b
+resto = a % b
+
+print('Soma: {soma}'
+      '\n Subtracao:{subtracao} '
+      '\n Divisão: {divisao}'
+      '\n Resto: {resto}'.format(soma=soma,
+                                  subtracao=subtracao,
+                                  divisao=divisao,
+                                  resto=resto))
